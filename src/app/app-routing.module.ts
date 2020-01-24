aadc11ccab430d991d589ceaf64460b4149961aa
alexb997 <alex_alexandru_997@gmail.com>
2020-01-24 14:46:06 +0200

    to the github

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListProductsComponent } from './list-products/list-products.component';
+import { AddProductComponent } from './add-product/add-product.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'add', component: AddProductComponent },
+  { path: 'list', component: ListProductsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddProductComponent } from './add-product/add-product.component';
+import { ListProductsComponent } from './list-products/list-products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddProductComponent,
+    ListProductsComponent
   ],
   imports: [
     BrowserModule,
